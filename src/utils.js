e5b41a43e619719e7a3dd018d1d477df886a80c3
MarcusEstr <estrada.marcus@gmail.com>
2018-11-05 06:43:38 -0800

    First commit

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+/* load_google_maps function via RyanWaite: https://github.com/ryanwaite28/script-store/blob/master/js/react_resolve_google_maps.js */
+
+export function load_google_maps() {
+  return new Promise(function(resolve, reject) {
+    // define the global callback that will run when google maps is loaded
+    window.resolveGoogleMapsPromise = function() {
+      // resolve the google object
+      resolve(window.google);
+      // delete the global callback to tidy up since it is no longer needed
+      delete window.resolveGoogleMapsPromise;
+    }
+    // Now, Load the Google Maps API
+    const script = document.createElement("script");
+    const API_KEY = 'AIzaSyAjhXB8E18pbk1cW3EpcC5HbCjMr5bowmU';
+    script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
+    script.async = true;
+    document.body.appendChild(script);
+  });
+}
