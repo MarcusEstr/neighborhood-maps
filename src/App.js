8748fa045f8d0d3d030146d9d858b41683250411
MarcusEstr <estrada.marcus@gmail.com>
2018-11-05 19:08:32 -0800

    Show map with markers on screen.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,31 @@ class App extends Component {
       googleMapsPromise, placesPromise
     ])
     .then(values => {
-      console.log(values);
+      // console.log(values);
+      let google = values[0]; //Google Maps
+      let venues = values[1].response.venues; //Foursquare
+      this.google = google;
+      this.markers = [];
+
+      this.map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8, 
+        scrollWheel: true,
+        center: {lat: venues[0].location.lat, lng: venues[0].location.lng}
+      });
+
+      venues.forEach(venue => {
+        let marker = new google.maps.Marker({
+          position: { lat: venue.location.lat, lng: venue.location.lng },
+          map: this.map,
+          venue: venue,
+          id: venue.id,
+          name: venue.name,
+          aniamtion: google.maps.Animation.DROP
+        });
+      });
+
     })
+
   }
 
   render() {
